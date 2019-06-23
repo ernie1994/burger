260fa3ffd536c7f3d372930f6b7651c11e0c45e3
garcie67 <ernie1994@gmail.com>
2019-06-23 14:46:24 -0700

    Modified connection config to connect to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 module.exports = mysql.createConnection({
-    host: "localhost",
-    port: 3307,
-    database: "burgers_db",
-    user: "root",
-    password: "root"
+    host: "y5s2h87f6ur56vae.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    database: "xq2a5dlffo7qndx4",
+    user: "simgqsctmcdu6wzb",
+    password: "w0s9ibe5kbsc6gd0"
 });
